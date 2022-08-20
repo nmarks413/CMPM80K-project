49d45222c457a5f2cc7d7faddd4635f327ae25b7
Nat <nmarks413@gmail.com>
2022-08-20 15:16:43 -0700

    audio

--- BurstYourBubbleV2/Assets/Scripts/Scene Management/Load/NextScene.cs
+++ BurstYourBubbleV2/Assets/Scripts/Scene Management/Load/NextScene.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NextScene : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    
+    private async void OnCollisionEnter2D(Collision2D collision)
     {
-        if(SceneManager.GetActiveScene().name == "Bedroom")
+        switch(PlayerPrefs.GetString("GameState"))
         {
-            SceneManager.LoadScene("School Hallway", LoadSceneMode.Single);
+            case "Awake":
+                AudioSource bus = Resources.Load<AudioSource>("Audio/bus.wav");
+                AudioSource kids = Resources.Load<AudioSource>("Audio/kids.wav");
+                bus.volume = .5f;
+                kids.volume = .5f;
+                bus.Play();
+                kids.Play();
+                await Task.Delay(2000);
+                bus.Stop();
+                kids.Stop();
+                SceneManager.LoadScene("School Hallway", LoadSceneMode.Single);
+                break;
+            case "":
+                break;
         }
     }
 }
