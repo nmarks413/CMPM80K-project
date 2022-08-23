57511fc20a378a29309c6841d1e688d9bee2b874
Nat <nmarks413@gmail.com>
2022-08-22 18:19:56 -0700

    healthbar

--- BurstYourBubbleV2/Assets/Scripts/confidence.cs
+++ BurstYourBubbleV2/Assets/Scripts/confidence.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class confidence : MonoBehaviour
 {
+    private Image bar;
     private int conf;
-
+    private int MaxConf;
     // Start is called before the first frame update
     void Start()
     {
-        conf = PlayerPrefs.GetInt("Confidence");
-        
-        
+        MaxConf = 100;
+        bar = GameObject.Find("ConfidenceBar").gameObject.GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        conf = PlayerPrefs.GetInt("Confidence");
+        bar.fillAmount = (float)conf / (float)MaxConf;
     }
 }
